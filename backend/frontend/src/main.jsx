bc62b27747274ff4103d6f22c0aee37097fe76cb
Ranjeet kumar <157788421+Ranjeet7680@users.noreply.github.com>
2025-10-15 14:39:39 +0530

    Update main.jsx

--- backend/frontend/src/main.jsx
+++ backend/frontend/src/main.jsx
@@ -1,1 +1,24 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import store from './store/store'
+import './index.css'
 
+
+const queryClient = new QueryClient()
+
+
+createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<Provider store={store}>
+<QueryClientProvider client={queryClient}>
+<BrowserRouter>
+<App />
+</BrowserRouter>
+</QueryClientProvider>
+</Provider>
+</React.StrictMode>
+)
